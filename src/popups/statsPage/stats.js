d5a4b92f7d90187f0f178c586c9393da1501f3fe
louismeunier <louismeunier123@gmail.com>
2020-12-16 21:53:12 -0500

    Changed logic to create Person, formed from other objects (ie Meta), rather than some convoluted string manipulation like before. Basically components :thumbsup:

--- src/popups/statsPage/stats.js
+++ src/popups/statsPage/stats.js
@@ -1,37 +1,59 @@
 class Meta {
+    /*
+    Constructs the meta section of each person's profile, including: name,picture,id,country,solves (#), and comps (#).
+    */
     constructor(info) {
-        //info should be the meta object for the desired person
         self.info = info;
+        self.elements = [];
+    }
+    baseElement(type,className=null,content=null) {
+        var newElement = document.createElement(type);
+        if (content) {
+            newElement.setAttribute("class",className);
+        }
+        if (className) {
+            newElement.innerText = content;
+        }
+        return newElement;
     }
     getMeta() {
         return this.generateMeta();
     }
     generateMeta() {
-        //add rest of attributes later if this method works
-        //maybe seperate each element into different methods?
-        var div = document.createElement("div");
-        div.className="meta";
-        var avatar = document.createElement("img");
+        //bug: avatar and meta div not getting classes assigned...
+        var div = this.baseElement("div","meta");
+        
+        var avatar = this.baseElement("img","avatar");
         avatar.src = self.info.avatar;
-        avatar.style.height = "45px";
-        var name = document.createElement("h1");
-        name.innerText = self.info.name;
+        avatar.style.height = "120px";
+        var id = this.baseElement("h2","wca-id",self.info.id);
+        var name = this.baseElement("h1","name",self.info.name);
+        var country = this.baseElement("h2","country",self.info.country);
+        //Is gender really necessary to have...?
+        var solvesPerComp = `${self.info.solveCount} solves, ${self.info.compCount} comps`;
+        var solvesPerCompElement = this.baseElement("h2","solves-per-comp",solvesPerComp);
+        div.insertAdjacentElement("afterbegin",solvesPerCompElement);
+        div.insertAdjacentElement("afterbegin",country);
         div.insertAdjacentElement("afterbegin",avatar);
+        div.insertAdjacentElement("afterbegin",id);
         div.insertAdjacentElement("afterbegin",name);
+        
         return div;
     }
 }
 
 class Person {
-    constructor(id) {
-        self.id=id;
-        self.meta = new Meta(self.id);
+    /*
+    Constructs the person
+    */
+    constructor(info) {
+        self.info=info;
+        self.meta = new Meta(self.info);
     }
     getPerson() {
         return this.generatePerson();
     }
     generatePerson() {
-
         var personDiv = document.createElement("div")
         personDiv.className = "person";
         personDiv.insertAdjacentElement("afterbegin",self.meta.generateMeta());
@@ -42,11 +64,19 @@ class Person {
 function initialize() {
     chrome.storage.local.get(["wcaData"], function(items) {
         console.log(items.wcaData);
-        for (var i=0;i<Object.keys(items.wcaData).length;i++) {
+        var people = [];
+        var keys = Object.keys(items.wcaData);
+        for (var i=0;i<keys.length;i++) {
             console.log(i);
-            //var meta = items.wcaData[Object.keys(items.wcaData)[i]]["meta"];
-            var person = new Person(items.wcaData[Object.keys(items.wcaData)[i]]["meta"]);
-            document.getElementById("stats").insertAdjacentElement("afterbegin",person.getPerson());
+            var person = new Person(items.wcaData[keys[i]]["meta"]);
+            people.push(person);
+            document.getElementById("stats").insertAdjacentElement("beforeend",person.getPerson());
+            //This logic adds a div between each element but NOT at the end of the last one or before the first
+            if (i!=keys.length-1) {
+                var barrier = document.createElement("div");
+                barrier.setAttribute("class","barrier");
+                document.getElementById("stats").insertAdjacentElement("beforeend",barrier);
+            } 
         }
     })
     
